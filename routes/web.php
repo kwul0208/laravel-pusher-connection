7ad6301ad20cdba514486ced3a527665e5c76ceb
ahmad_wahyu <kwul0208@gmail.com>
2022-07-27 17:36:49 +0700

    ep 1 = pengenalan broadcasting

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\MessageCreated;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    MessageCreated::dispatch('test message');
+    // event(event: new MessageCreated);
+
     return view('welcome');
 });
